0fae9638bef921a85a144137b38ff2eb4e68eb4f
Peculiar <onugbolup2019@gmail.com>
2022-08-16 09:06:23 +0100

    Add project files.

---
+++ Delegate2/Delegates2.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegate2
+{
+    internal class Delegate
+    {
+        public delegate void DataUpload(int percentDone);
+        public void iCloudSaver(DataUpload DU )
+        {
+                for (int i = 0; i < 50000; i++)
+                {
+                    var Uploaded = i.ToString();
+                    DU(i);
+
+                }
+        }
+
+                
+
+    }
+}
+
---
+++ Delegate2/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Delegate2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            var Delegate  = new Delegate();
+            Delegate.iCloudSaver(DataUpload);
+        }
+        public static void DataUpload(int DU)
+        {
+            Console.WriteLine($"so far {DU} done");
+ 
+    }   }
+}
